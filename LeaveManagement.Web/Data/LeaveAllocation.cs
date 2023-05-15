35b094885ca96f416d6bbf89e2267c62d8819672
erdemerencen <erdemerencen@gmail.com>
2023-05-15 16:19:38 +0300

    Tables and migrations

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int Id { get; set; }
+        public int NumberofDays { get; set; }
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeavetypeId { get; set; }
+
+        public string EmployeeId { get; set; }
+
+        
+
+    }
+}
